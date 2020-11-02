fe62de636c1eeb1f100f22e870ac8dc7f8bab135
Shivam Sharma <sshivam@umich.edu>
2020-11-03 01:25:20 +0530

    remove debug prints

--- redbrick/entity/export/export_video.py
+++ redbrick/entity/export/export_video.py
@@ -102,7 +102,6 @@ class ExportVideo(ExportBase):
                 )
 
                 frame_data = np.flip(url_to_image(item), axis=2)
-                print("IMAGE FILEPATH", image_filepath)
                 cv2.imwrite(image_filepath, frame_data)
 
                 # create the label file name
