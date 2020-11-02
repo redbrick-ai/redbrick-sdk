8da5b3398c5d0dd4b2a87b7a819077f29b2f9128
Shivam Sharma <sshivam@umich.edu>
2020-11-03 01:59:45 +0530

    v0.2.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
     name="redbrick-sdk",
     url="https://github.com/dereklukacs/redbrick-sdk",
-    version="0.2.7",
+    version="0.2.8",
     description="RedBrick platform python SDK!",
     py_modules=["redbrick"],
     packages=find_packages(),
