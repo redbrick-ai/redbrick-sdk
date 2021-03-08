1e24142dc78c7f44e4c3cea68e6274b68483bfec
Derek <dlukacs@umich.edu>
2021-03-08 10:45:58 -0500

    fix download images now has same filename as labels

--- redbrick/export/export_new.py
+++ redbrick/export/export_new.py
@@ -88,7 +88,12 @@ class ExportRedbrick:
         self.label_set_name = label_set_name
         self.target_dir = target_dir
 
-    def export(self, download_data: bool = False, single_json: bool = False, use_name: bool = False) -> None:
+    def export(
+        self,
+        download_data: bool = False,
+        single_json: bool = False,
+        use_name: bool = False,
+    ) -> None:
         # Create LabelsetLabelsIterator
         labelsetIter = LabelsetLabelsIterator(
             org_id=self.org_id, label_set_name=self.label_set_name
@@ -98,23 +103,25 @@ class ExportRedbrick:
         if not os.path.exists(self.target_dir):
             os.makedirs(self.target_dir)
 
-        #Create folder 'data' inside target_dir
+        # Create folder 'data' inside target_dir
         if download_data:
             target_data_dir = os.path.join(self.target_dir, "data")
             if not os.path.exists(target_data_dir):
                 os.makedirs(target_data_dir)
 
         if download_data:
-            print_info("Exporting datapoints and data to dir: {}".format(self.target_dir))
+            print_info(
+                "Exporting datapoints and data to dir: {}".format(self.target_dir)
+            )
         else:
             print_info("Exporting datapoints to dir: {}".format(self.target_dir))
 
-        #If single json file
+        # If single json file
         if single_json:
             dpoints_flat = []
 
         for dpoint in tqdm(labelsetIter, total=labelsetIter.datapointCount):
-            #saving datapoint to json
+            # saving datapoint to json
             dpoint_flat = {
                 "dpId": dpoint["dpId"],
                 "items": dpoint["items"],
@@ -123,114 +130,116 @@ class ExportRedbrick:
                 "labels": dpoint["labelData"]["labels"],
             }
 
-            #Check if current datapoint is a video
+            # Check if current datapoint is a video
             IS_VIDEO = False
             if len(dpoint["items"]) > 1:
-                #This is a video
+                # This is a video
                 IS_VIDEO = True
 
-            #If single json is required, append current datapoint to list
+            # If single json is required, append current datapoint to list
             if single_json:
                 dpoints_flat.append(dpoint_flat)
             else:
-                #Check if use_name is needed
+                # Check if use_name is needed
                 if use_name and dpoint["name"] is not None and dpoint["name"] is not "":
                     fname = dpoint["name"]
                     last_slash = fname.rfind("/")
-                    fname = clear_url(fname[last_slash+1:])
+                    fname = clear_url(fname[last_slash + 1 :])
                     file_ext = fname[-4:]
                     if file_ext.lower() in [".jpg", ".png", ".bmp"]:
-                        #If image name already has an image extension
+                        # If image name already has an image extension
                         fname = fname.replace(file_ext.lower(), "")
                 else:
                     fname = dpoint["dpId"]
-                #Json path to store current datapoint
-                jsonPath = os.path.join(
-                    self.target_dir, "{}.json".format(fname)
-                )
-                #Check for collisions
+                # Json path to store current datapoint
+                jsonPath = os.path.join(self.target_dir, "{}.json".format(fname))
+                # Check for collisions
                 idx = 1
                 while True:
                     if not os.path.exists(jsonPath):
                         break
                     jsonPath = os.path.join(
-                        self.target_dir, 
-                        "{}({}).json".format(fname, idx)
+                        self.target_dir, "{}({}).json".format(fname, idx)
                     )
                     idx += 1
 
-                #Save current datapoint to json file
+                # Save current datapoint to json file
                 with open(jsonPath, mode="w", encoding="utf-8") as f:
                     json.dump(dpoint_flat, f, indent=2)
 
-            #Saving datapoint image/video data
+            # Saving datapoint image/video data
             if download_data:
                 if not IS_VIDEO:
-                    #Handling of image filepath
-                    dpoint_item = dpoint["items"][0]
+                    # Handling of image filepath
+                    dpoint_item = dpoint["itemsPresigned"][0]
                     if use_name:
                         dpoint_name = dpoint["name"]
                     else:
-                        dpoint_name = dpoint_item
+                        dpoint_name = dpoint["dpId"]
                     last_slash = dpoint_name.rfind("/")
-                    #Replacing special characters on file path
-                    image_name = clear_url(dpoint_name[last_slash+1:])
+                    # Replacing special characters on file path
+                    image_name = clear_url(dpoint_name[last_slash + 1 :])
                     file_ext = image_name[-4:]
                     if file_ext.lower() in [".jpg", ".png", ".bmp"]:
-                        #If image name already has an image extension
+                        # If image name already has an image extension
                         image_filepath = os.path.join(target_data_dir, image_name)
                     else:
-                        image_filepath = os.path.join(target_data_dir, image_name + ".jpg")
-                    #Downloading image from url as numpy array
+                        image_filepath = os.path.join(
+                            target_data_dir, image_name + ".jpg"
+                        )
+                    # Downloading image from url as numpy array
                     image_np = url_to_image(dpoint_item)
-                    #Check for collisions
+                    # Check for collisions
                     idx = 1
                     while True:
                         if not os.path.exists(image_filepath):
                             break
                         if file_ext.lower() in [".jpg", ".png", ".bmp"]:
-                            #If image name already has an image extension
-                            image_filepath = os.path.join(target_data_dir, image_name[:-4] + "({})".format(idx) + image_name[-4:])
+                            # If image name already has an image extension
+                            image_filepath = os.path.join(
+                                target_data_dir,
+                                image_name[:-4] + "({})".format(idx) + image_name[-4:],
+                            )
                         else:
-                            image_filepath = os.path.join(target_data_dir, image_name + "({}).jpg".format(idx))
+                            image_filepath = os.path.join(
+                                target_data_dir, image_name + "({}).jpg".format(idx)
+                            )
                         idx += 1
-                    #Saving image to specified path
+                    # Saving image to specified path
                     cv2.imwrite(  # pylint: disable=no-member
                         image_filepath, np.flip(image_np, axis=2)
                     )
                 else:
-                    #Video name
+                    # Video name
                     video_name = dpoint["name"]
-                    #Create folder to store frames
+                    # Create folder to store frames
                     target_data_video_dir = os.path.join(target_data_dir, video_name)
                     if not os.path.exists(target_data_video_dir):
                         os.makedirs(target_data_video_dir)
-                    #Store frames
+                    # Store frames
                     idx = 1
                     for i in range(len(dpoint["items"])):
                         dpoint_item = dpoint["items"][i]
                         file_ext = dpoint_item[-4:]
                         if file_ext.lower() in [".jpg", ".png", ".bmp"]:
-                            #If image name already has an image extension
+                            # If image name already has an image extension
                             image_name = str(idx).zfill(6) + file_ext.lower()
                         else:
                             image_name = str(idx).zfill(6) + ".jpg"
                         idx += 1
                         image_filepath = os.path.join(target_data_video_dir, image_name)
-                        #Downloading image from url as numpy array
+                        # Downloading image from url as numpy array
                         image_np = url_to_image(dpoint_item)
-                        #Saving image to specified path
+                        # Saving image to specified path
                         cv2.imwrite(  # pylint: disable=no-member
                             image_filepath, np.flip(image_np, axis=2)
                         )
 
-        #Saving all json data in a single file
+        # Saving all json data in a single file
         if single_json:
             jsonPath = os.path.join(
-                self.target_dir, "{}_{}.json".format(
-                    self.org_id,
-                    self.label_set_name.replace(" ","-")
-                )
+                self.target_dir,
+                "{}_{}.json".format(self.org_id, self.label_set_name.replace(" ", "-")),
             )
             with open(jsonPath, mode="w", encoding="utf-8") as f:
                 json.dump(dpoints_flat, f, indent=2)
