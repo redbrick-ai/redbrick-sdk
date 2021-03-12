a661bab1a3afe3abb425fb50a00f5fdf0d953320
Derek <dlukacs@umich.edu>
2021-03-12 11:00:32 -0500

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
     name="redbrick-sdk",
     url="https://github.com/dereklukacs/redbrick-sdk",
-    version="0.3.0",
+    version="0.3.2",
     description="RedBrick platform python SDK!",
     py_modules=["redbrick"],
     packages=find_packages(),
