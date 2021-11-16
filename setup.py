ffb97054c73442b7dd00b73441c6a56216a71050
Shivam Sharma <sshivam@umich.edu>
2021-11-16 16:47:56 +0530

    updates to setup.py

--- setup.py
+++ setup.py
@@ -13,36 +13,6 @@ with open(
 ) as f:
     version = f.read().strip()
 
-# handle different OS's and python versions
-
-install_requires = [
-    "requests==2.23.0",
-    "tqdm==4.50.0",
-    "termcolor==1.1.0",
-    "requests==2.23.0",
-    "aiohttp==3.7.4",
-    "cchardet==2.1.7",
-    "aiodns==3.0.0",
-    "Pillow==8.3.2",
-    "shapely==1.7.1",
-    "numpy>=1.15",
-    "matplotlib>=3.2",
-    "scikit-image==0.18.3",
-    "pyparsing==2.4.7",
-]
-
-import sys
-
-if sys.platform in ("darwin", "linux"):
-    install_requires.append("rasterio==1.2.10")
-
-if sys.platform in ("win32",):
-
-    if sys.version_info == (3, 8):
-        install_requires.append("GDAL-3.3.3-cp38-cp38-win_amd64.whl")
-        install_requires.append("rasterio-1.2.10-cp38-cp38-win_amd64.whl")
-
-
 setup(
     name="redbrick-sdk",
     url="https://github.com/redbrick-ai/redbrick-sdk",
@@ -54,7 +24,21 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
-    install_requires=install_requires,
+    install_requires=[
+        "requests==2.23.0",
+        "tqdm==4.50.0",
+        "termcolor==1.1.0",
+        "requests==2.23.0",
+        "aiohttp==3.7.4",
+        "cchardet==2.1.7",
+        "aiodns==3.0.0",
+        "Pillow==8.3.2",
+        "shapely==1.7.1",
+        "numpy>=1.15",
+        "rasterio==1.2.10",
+        "matplotlib>=3.2",
+        "scikit-image==0.18.3",
+    ],
     extras_require={
         "dev": [
             "twine==3.1.1",
