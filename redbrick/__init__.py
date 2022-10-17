23c587d546660580dd5011316a5442291e4b880a
Pritam Rungta <pritamrungta@gmail.com>
2022-10-17 20:36:26 +0530

    project members list

--- redbrick/__init__.py
+++ redbrick/__init__.py
@@ -27,7 +27,7 @@ from redbrick.utils.logging import print_info
 
 from .version_check import version_check
 
-__version__ = "2.3.0"
+__version__ = "2.3.1"
 
 # windows event loop close bug https://github.com/encode/httpx/issues/914#issuecomment-622586610
 try:
