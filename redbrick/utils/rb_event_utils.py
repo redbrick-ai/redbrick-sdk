724831fb9240539581ce5213c10f8a76ae8c67fb
Pritam Rungta <pritamrungta@gmail.com>
2022-12-13 20:50:41 +0530

    change task events report userId to email

--- redbrick/utils/rb_event_utils.py
+++ redbrick/utils/rb_event_utils.py
@@ -4,7 +4,7 @@ from typing import Optional, List, Dict
 from redbrick.common.enums import TaskEventTypes
 
 
-def user_format(user: Optional[str]) -> Optional[str]:
+def user_format(user: Optional[str], users: Dict[str, str]) -> Optional[str]:
     """User format."""
     if not user:
         return user
@@ -12,22 +12,22 @@ def user_format(user: Optional[str]) -> Optional[str]:
         return "System"
     if user.startswith("API:"):
         return "API Key"
-    return user
+    return users.get(user, user)
 
 
-def comment_format(comment: Dict) -> Dict:
+def comment_format(comment: Dict, users: Dict[str, str]) -> Dict:
     """Comment format."""
     return {
         "commentId": comment["commentId"],
         "commentText": comment["textVal"],
-        "createdBy": user_format(comment["createdBy"]["userId"]),
+        "createdBy": user_format(comment["createdBy"]["userId"], users),
         "stage": comment["stageName"],
         "isIssue": comment["issueComment"],
         "issueResolved": comment["issueResolved"],
     }
 
 
-def task_event_format(task: Dict) -> Dict:
+def task_event_format(task: Dict, users: Dict[str, str]) -> Dict:
     """Convert task to event format."""
     # pylint: disable=too-many-branches, too-many-statements
     events: List[Dict] = []
@@ -45,7 +45,7 @@ def task_event_format(task: Dict) -> Dict:
                 event["isGroundTruth"] = task_event["createEvent"]["isGroundTruth"]
                 if task_event["taskData"]:
                     event["createdBy"] = user_format(
-                        task_event["taskData"]["createdBy"]
+                        task_event["taskData"]["createdBy"], users
                     )
                 prev_stage = task_event["createEvent"]["currentStageName"]
             elif task_event.get("inputEvent"):
@@ -62,7 +62,7 @@ def task_event_format(task: Dict) -> Dict:
                     if task_event["outputEvent"]["currentStageName"] == "END":
                         event_type = TaskEventTypes.GROUNDTRUTH_TASK_EDITED
                         event["updatedBy"] = user_format(
-                            task_event["taskData"]["createdBy"]
+                            task_event["taskData"]["createdBy"], users
                         )
                     else:
                         event_type = (
@@ -74,14 +74,16 @@ def task_event_format(task: Dict) -> Dict:
                         )
                         event["stage"] = task_event["outputEvent"]["currentStageName"]
                         event["updatedBy"] = user_format(
-                            prev_assignee or task_event["taskData"]["createdBy"]
+                            prev_assignee or task_event["taskData"]["createdBy"], users
                         )
                 prev_stage = task_event["outputEvent"]["currentStageName"]
 
         elif task_event["__typename"] == "Comment":
             event_type = TaskEventTypes.COMMENT_ADDED
-            event.update(comment_format(task_event))
-            event["replies"] = list(map(comment_format, task_event["replies"]))
+            event.update(comment_format(task_event, users))
+            event["replies"] = [
+                comment_format(reply, users) for reply in task_event["replies"]
+            ]
             prev_stage = event["stage"]
         elif task_event["__typename"] == "TaskStateChanges":
             if (
@@ -92,14 +94,14 @@ def task_event_format(task: Dict) -> Dict:
             ):
                 event_type = TaskEventTypes.CONSENSUS_TASK_EDITED
                 event["consensusUsers"] = [
-                    {"assignee": user_format(assignee), "status": status}
+                    {"assignee": user_format(assignee, users), "status": status}
                     for assignee, status in zip(
                         task_event["consensusAssigneesAfter"],
                         task_event["consensusStatusesAfter"],
                     )
                 ]
                 event["prevConsensusUsers"] = [
-                    {"assignee": user_format(assignee), "status": status}
+                    {"assignee": user_format(assignee, users), "status": status}
                     for assignee, status in zip(
                         task_event["consensusAssigneesBefore"],
                         task_event["consensusStatusesBefore"],
@@ -109,24 +111,36 @@ def task_event_format(task: Dict) -> Dict:
             elif task_event["assignedToBefore"] != task_event["assignedToAfter"]:
                 if not task_event["assignedToAfter"]:
                     event_type = TaskEventTypes.TASK_UNASSIGNED
-                    event["prevAssignee"] = user_format(task_event["assignedToBefore"])
+                    event["prevAssignee"] = user_format(
+                        task_event["assignedToBefore"], users
+                    )
                 elif task_event["assignedToBefore"]:
                     event_type = TaskEventTypes.TASK_REASSIGNED
-                    event["assignee"] = user_format(task_event["assignedToAfter"])
-                    event["prevAssignee"] = user_format(task_event["assignedToBefore"])
+                    event["assignee"] = user_format(
+                        task_event["assignedToAfter"], users
+                    )
+                    event["prevAssignee"] = user_format(
+                        task_event["assignedToBefore"], users
+                    )
                 else:
                     event_type = TaskEventTypes.TASK_ASSIGNED
-                    event["assignee"] = user_format(task_event["assignedToAfter"])
+                    event["assignee"] = user_format(
+                        task_event["assignedToAfter"], users
+                    )
 
             elif task_event["statusBefore"] != task_event["statusAfter"]:
                 if task_event["statusAfter"] == "SKIPPED":
                     event_type = TaskEventTypes.TASK_SKIPPED
-                    event["assignee"] = user_format(task_event["assignedToAfter"])
+                    event["assignee"] = user_format(
+                        task_event["assignedToAfter"], users
+                    )
                 elif task_event["statusAfter"] == "IN_PROGRESS" and task_event[
                     "statusBefore"
                 ] in ("SKIPPED", "ASSIGNED"):
                     event_type = TaskEventTypes.TASK_SAVED
-                    event["assignee"] = user_format(task_event["assignedToAfter"])
+                    event["assignee"] = user_format(
+                        task_event["assignedToAfter"], users
+                    )
                     if type_pos.get(TaskEventTypes.TASK_SAVED, -1) >= 0:
                         events.pop(type_pos[TaskEventTypes.TASK_SAVED])
                         last_pos -= 1
@@ -155,6 +169,7 @@ def task_event_format(task: Dict) -> Dict:
 
     return {
         "taskId": task["taskId"],
+        "taskName": task["datapoint"]["name"],
         "currentStageName": task["currentStageName"],
         "events": sorted(events, key=lambda evt: evt.get("createdAt", "") or ""),
     }
