9e5d7d0428cebc10246041edb2783131eac4ab07
Pritam Rungta <59230352+pritamrungta@users.noreply.github.com>
2023-10-19 08:17:23 +0530

    bump version :: 2.13.16

--- redbrick/__init__.py
+++ redbrick/__init__.py
@@ -28,7 +28,7 @@ from redbrick.utils.common_utils import config_migration
 
 from .version_check import version_check
 
-__version__ = "2.13.16b2"
+__version__ = "2.13.16"
 
 # windows event loop close bug https://github.com/encode/httpx/issues/914#issuecomment-622586610
 try:
