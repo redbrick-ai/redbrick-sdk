69f745613e392b91a251eca688bb07240b0073a3
cAtaman <chimaataman@gmail.com>
2023-10-29 08:37:01 +0100

    remove commented code

--- redbrick/utils/dicom.py
+++ redbrick/utils/dicom.py
@@ -633,7 +633,6 @@ def merge_rtstructs(rtstruct1: Any, rtstruct2: Any) -> Any:
         # check for ROI name duplication
         for struct_set_roi_seq2 in rtstruct2.ds.StructureSetROISequence:
             if struct_set_roi_seq.ROIName == struct_set_roi_seq2.ROIName:
-                # struct_set_roi_seq += "_2"
                 struct_set_roi_seq.ROIName += "_2"
 
         rtstruct2.ds.ROIContourSequence.append(roi_contour_seq)
