aa1a40230a154ec21590333c382b11bbecc5579e
cAtaman <chimaataman@gmail.com>
2023-12-04 13:05:58 +0100

    add fixture

--- tests/test_cli/conftest.py
+++ tests/test_cli/conftest.py
@@ -2,8 +2,11 @@
 import os
 import shutil
 import typing as t
+import uuid
 
 import pytest
+
+from tests.test_cli import _write_config, _write_creds
 
 
 @pytest.fixture
@@ -20,3 +23,15 @@ def project_and_conf_dirs(
     yield project_path, credentials_path
     shutil.rmtree(project_path, ignore_errors=True)
     shutil.rmtree(credentials_path, ignore_errors=True)
+
+
+@pytest.fixture
+def prepare_project(project_and_conf_dirs, rb_context_full):
+    project_path, config_path_ = project_and_conf_dirs
+    org_id = str(uuid.uuid4())
+    project_id = str(uuid.uuid4())
+
+    # prepare project config and creds
+    _write_config(project_path, org_id, project_id=project_id)
+    _write_creds(config_path_, org_id, api_key=rb_context_full.client.api_key)
+    return project_path, config_path_, org_id, project_id
