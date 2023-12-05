39fd89db1f621e523e12d133245bf5cfbe414310
cAtaman <chimaataman@gmail.com>
2023-12-05 16:57:43 +0100

    add test for upload command

--- tests/test_cli/conftest.py
+++ tests/test_cli/conftest.py
@@ -1,12 +1,17 @@
 """Pytest Fixtures for tests in test.test_cli"""
+import argparse
+import functools
 import os
 import shutil
 import typing as t
 import uuid
+from datetime import datetime
+from unittest.mock import patch
 
 import pytest
 
-from tests.test_cli import _write_config, _write_creds
+from redbrick.cli import public
+from tests.test_cli import _write_config, _write_creds, mock_method
 
 
 @pytest.fixture
@@ -38,3 +43,59 @@ def prepare_project(
     _write_config(project_path, org_id, project_id=project_id)
     _write_creds(config_path_, org_id, api_key=rb_context_full.client.api_key)
     return project_path, config_path_, org_id, project_id
+
+
+@pytest.fixture
+def mock_upload_controller(
+    prepare_project,  # pylint: disable=redefined-outer-name
+    monkeypatch,
+    rb_context_full,
+):
+    """Prepare a test CLIUploadController object"""
+    project_path, config_path_, org_id, project_id = prepare_project
+    monkeypatch.chdir(project_path)
+    _, cli = public.cli_parser(only_parser=False)
+
+    project_name = "real_project"
+
+    # mock repo methods
+    # pylint: disable=protected-access
+    mock_org_resp = {"name": "Mock Org", "orgId": org_id}
+    mock_projects_resp = [
+        {
+            "status": "CREATION_SUCCESS",
+            "projectId": project_id,
+            "name": project_name,
+            "tdType": "mock_tdType",
+            "taxonomy": {"name": "mock_taxonomy"},
+            "workspace": {"workspaceId": uuid.uuid4()},
+            "projectUrl": "mock_project_url",
+            "createdAt": datetime.now().isoformat(),
+            "consensusSettings": {"enabled": True},
+        }
+    ]
+    rb_context_full.project.get_org = functools.partial(
+        mock_method, response=mock_org_resp
+    )
+    rb_context_full.project.get_project = functools.partial(
+        mock_method, response=mock_projects_resp[0]
+    )
+    rb_context_full.project.get_projects = functools.partial(
+        mock_method, response=mock_projects_resp
+    )
+    rb_context_full.project.get_stages = functools.partial(mock_method, response=[])
+    # pylint: enable=protected-access
+
+    handle_upload = cli.upload.handle_upload
+    with (
+        patch("redbrick.cli.project.config_path", return_value=config_path_),
+        patch("redbrick.cli.command.clone.CLIProject._context", rb_context_full),
+        patch.object(cli.upload, "handle_upload"),
+    ):
+        args = argparse.Namespace(command=cli.CLONE)
+        cli.upload.handler(args)
+        _ = cli.upload.project.project
+        controller = cli.upload
+
+    controller.handle_upload = handle_upload
+    return controller, project_path
--- tests/test_cli/test_command/test_upload.py
+++ tests/test_cli/test_command/test_upload.py
@@ -1,9 +1,12 @@
 """Tests for `redbrick.cli.command.upload`."""
 import argparse
+import json
+import os.path
 from unittest.mock import patch
 
 import pytest
 
+from redbrick import ImportTypes
 from redbrick.cli import public, CLIProject
 
 
@@ -21,3 +24,92 @@ def test_handler(prepare_project, monkeypatch):
         cli.upload.handler(args)
         _handle_upload.assert_called_once()
         assert isinstance(cli.upload.project, CLIProject)
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    ("is_json", "use_dir", "error_msg"),
+    [
+        (True, False, 'Could not read json file, with "json=True"'),
+        (True, True, "Could not find json file, in directory"),
+        (False, True, "Could not find files in directory"),
+    ],
+)
+def test_handle_upload_json(
+    mock_upload_controller, monkeypatch, is_json, use_dir, error_msg
+):  # pylint: disable=too-many-locals
+    """Test the `CLIUploadController.handle_upload` when on different
+    values of the "json" argument
+    """
+    controller, project_path = mock_upload_controller
+    monkeypatch.chdir(project_path)
+
+    json_filepath = os.path.join(project_path, "test.json")
+    label_filepath = os.path.join(project_path, "test_label.json")
+    dicom_filepath = os.path.join(project_path, "test_file.dcm")
+
+    mock_json_data = [
+        {
+            "name": "",
+            "segmentations": {"0": label_filepath},
+            "items": [dicom_filepath],
+        },
+    ]
+
+    with open(dicom_filepath, "wb") as file:
+        file.write(b"stuff")
+
+    with open(json_filepath, "w", encoding="utf-8") as file:
+        json.dump(mock_json_data, file)
+
+    with open(json_filepath, "w", encoding="utf-8") as file:
+        json.dump(mock_json_data, file)
+
+    # pylint: disable=unused-argument
+    async def mock_validate_json(ctx, file_data, *args):
+        return file_data
+
+    async def mock_create_tasks(s_id, points, *args):
+        items = []
+        for point in points:
+            _items = point["items"]
+            for _item in _items:
+                items.append({"response": None, "name": _item})
+        return items
+
+    async def mock_gen_item_list(self, item_list, *args):
+        return item_list
+
+    # pylint: enable=unused-argument
+
+    with (
+        patch("redbrick.upload.public.validate_json", mock_validate_json),
+        patch.object(
+            controller.project.project.upload, "_create_tasks", mock_create_tasks
+        ),
+        patch.object(
+            controller.project.project.upload, "generate_items_list", mock_gen_item_list
+        ),
+    ):
+        _dir = project_path if use_dir else json_filepath
+        controller.args = argparse.Namespace(
+            directory=_dir,
+            json=is_json,
+            type=ImportTypes.DICOM3D,
+            as_study=False,
+            as_frames=True,
+            segment_map=None,
+            storage=controller.STORAGE_REDBRICK,
+            label_storage=None,
+            ground_truth=False,
+            label_validate=False,
+            clear_cache=False,
+            concurrency=10,
+        )
+        controller.handle_upload()
+        upload_cache_hash = controller.project.conf.get_option("uploads", "cache")
+        upload_cache = controller.project.cache.get_data(
+            "uploads", upload_cache_hash, fixed_cache=True
+        )
+        assert isinstance(upload_cache_hash, str), error_msg
+        assert upload_cache is not None, error_msg
