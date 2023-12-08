baa2d4fbb6f37cfce215da5ca06ee238b1906b71
cAtaman <chimaataman@gmail.com>
2023-12-08 20:31:02 +0100

    mock windows console buffer

--- tests/test_cli/test_input/conftest.py
+++ tests/test_cli/test_input/conftest.py
@@ -1,4 +1,5 @@
 """Pytest fixtures for tests.test_cli.test_input"""
+import sys
 from unittest.mock import patch
 
 import pytest
@@ -7,6 +8,11 @@ import pytest
 @pytest.fixture
 def mock_input_executor():
     """Prepare a mock input prompt"""
+    is_windows = sys.platform == "win32"
+
     with patch("InquirerPy.prompts.input.InputPrompt.execute") as mock_execute:
-        with patch("InquirerPy.prompts.input.PromptSession"):
+        if is_windows:
+            with patch("windll.kernel32.GetConsoleScreenBufferInfo", return_value=True):
+                yield mock_execute
+        else:
             yield mock_execute
