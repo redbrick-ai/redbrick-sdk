da19352252308e3fc7a532f33108a4f72e203cbe
cAtaman <chimaataman@gmail.com>
2023-12-08 20:21:43 +0100

    mock PromptSession

--- tests/test_cli/test_input/conftest.py
+++ tests/test_cli/test_input/conftest.py
@@ -7,5 +7,6 @@ import pytest
 @pytest.fixture
 def mock_input_executor():
     """Prepare a mock input prompt"""
-    with patch("InquirerPy.prompts.input.InputPrompt.__new__") as mock_prompt:
-        yield mock_prompt.return_value.execute
+    with patch("InquirerPy.prompts.input.InputPrompt.execute") as mock_execute:
+        with patch("InquirerPy.prompts.input.PromptSession"):
+            yield mock_execute
