25c7da515f359559dbb923a2a6ee916bb59db5d6
cAtaman <chimaataman@gmail.com>
2023-12-08 20:05:46 +0100

    patch InputPromt.__new__

--- tests/test_cli/test_input/conftest.py
+++ tests/test_cli/test_input/conftest.py
@@ -7,5 +7,5 @@ import pytest
 @pytest.fixture
 def mock_input_executor():
     """Prepare a mock input prompt"""
-    with patch("InquirerPy.prompts.input.InputPrompt.execute") as mock_executor:
-        yield mock_executor
+    with patch("InquirerPy.prompts.input.InputPrompt.__new__") as mock_prompt:
+        yield mock_prompt.return_value.execute
