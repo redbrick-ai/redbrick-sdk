72047cd5a05f37657c24be51c0223154c70c81d8
Pritam Rungta <59230352+pritamrungta@users.noreply.github.com>
2024-10-09 12:40:12 +0530

    bump version :: v2.19.0

--- redbrick/__init__.py
+++ redbrick/__init__.py
@@ -30,7 +30,7 @@ from .config import config
 from .version_check import version_check
 
 
-__version__ = "2.18.11"
+__version__ = "2.19.0"
 
 # windows event loop close bug https://github.com/encode/httpx/issues/914#issuecomment-622586610
 try:
