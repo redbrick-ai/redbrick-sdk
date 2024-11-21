12067d8b669a06ca73e1cc0a8ef84094b624b3f8
Avanish Gupta <avanishcodes+github@gmail.com>
2024-11-21 14:13:36 +0530

    fix: Use AltaDB as a dependency

--- redbrick/utils/files.py
+++ redbrick/utils/files.py
@@ -7,10 +7,6 @@ from typing import Any, Dict, List, Optional, Tuple, Set
 import asyncio
 import aiohttp
 
-import pydicom
-import pydicom.dataset
-import pydicom.encaps
-import pydicom.uid
 
 from yarl import URL
 from tenacity import Retrying, RetryError
@@ -20,13 +16,13 @@ from tenacity.wait import wait_random_exponential
 from natsort import natsorted, ns
 
 from redbrick.common.constants import (
-    MAX_CONCURRENCY,
     MAX_FILE_BATCH_SIZE,
     MAX_RETRY_ATTEMPTS,
 )
 from redbrick.utils.async_utils import gather_with_concurrency
 from redbrick.utils.logging import log_error, logger
 from redbrick.config import config
+from altadb.utils.files import save_dicom_series
 
 
 IMAGE_FILE_TYPES = {
@@ -326,163 +322,3 @@ async def download_files(
 
     await asyncio.sleep(0.250)  # give time to close ssl connections
     return [(path if isinstance(path, str) else None) for path in paths]
-
-
-async def save_dicom_series(
-    altadb_meta_content_url: str,
-    dataset_root: str,
-    series_id: str,
-    base_url: str = "https://app.altadb.com",
-    headers: Optional[Dict[str, str]] = None,
-) -> List[str]:
-    """Save DICOM files using AltaDB URLs.
-    Given an AltaDB URL containing the metadata and image frames.
-
-    Save the DICOM files to the destination directory.
-    One DICOM file can contain multiple image frames.
-
-    Args
-    ------------
-    altadb_meta_content_url: str
-        AltaDB URL containing the metadata and image frames.
-        This URL can be signed or unsigned.
-    base_dir: str
-        Destination directory to save the DICOM files.
-    base_url: str
-        Base URL for the AltaDB API.
-    headers: Optional[Dict[str, str]]
-        Headers to be used for the HTTP requests.
-        If the altaDB_meta_content_url is unsigned, the headers should contain the authorization token.
-    """
-    # pylint: disable=too-many-locals
-    series_dir = os.path.join(dataset_root, series_id)
-    os.makedirs(series_dir, exist_ok=True)
-
-    async def save_dicom_dataset(
-        instance_metadata: Dict,
-        instance_frames_metadata: List[Dict],
-        presigned_image_urls: List[str],
-        destination_file: str,
-        aiosession: aiohttp.ClientSession,
-    ) -> None:
-        """Create and save a DICOM dataset using metadata and image frame URLs.
-
-        Args
-        ------------
-        instance_metadata: Dict
-            Metadata of the instance.
-        instance_frames_metadata: List[Dict]
-            Metadata of the instance frames.
-        presigned_image_urls: List[str]
-            Presigned URLs of the image frames.
-        destination_file: str
-            Destination file to save the DICOM dataset.
-        aiosession: aiohttp.ClientSession
-            aiohttp ClientSession to be used for the HTTP requests.
-        """
-
-        async def get_image_content(
-            aiosession: aiohttp.ClientSession, image_url: str
-        ) -> bytes:
-            """Get image content."""
-            async with aiosession.get(image_url) as response:
-                return await response.content.read()
-
-        frame_contents = await gather_with_concurrency(
-            MAX_FILE_BATCH_SIZE,
-            [
-                get_image_content(aiosession, image_url=image_frame_url)
-                for image_frame_url in presigned_image_urls
-            ],
-        )
-
-        ds_file = pydicom.Dataset.from_json(instance_metadata)
-        ds_file.TransferSyntaxUID = pydicom.uid.UID(
-            instance_frames_metadata[0]["metaData"]["00020010"]["Value"][0]
-        )
-
-        # Move the file meta information to the dataset file meta
-        if not hasattr(ds_file, "file_meta"):
-            ds_file.file_meta = pydicom.dataset.FileMetaDataset()
-
-        # Iterate through the dataset and copy Group 2 elements
-        for elem in ds_file:
-            if elem.tag.group == 2:  # Check if the element belongs to Group 2
-                ds_file.file_meta.add_new(elem.tag, elem.VR, elem.value)
-                del ds_file[elem.tag]  # Remove the element from the dataset
-
-        ds_file.PixelData = pydicom.encaps.encapsulate(frame_contents)
-
-        # PATCH/START: add HTJ2KLosslessRPCL to pydicom
-        from pydicom.uid import (  # pylint: disable=import-outside-toplevel
-            UID_dictionary,
-            AllTransferSyntaxes,
-            JPEG2000TransferSyntaxes,
-        )
-
-        HTJ2KLosslessRPCL = pydicom.uid.UID(  # pylint: disable=invalid-name
-            "1.2.840.10008.1.2.4.202"
-        )
-        AllTransferSyntaxes.append(HTJ2KLosslessRPCL)
-        JPEG2000TransferSyntaxes.append(HTJ2KLosslessRPCL)
-        UID_dictionary[HTJ2KLosslessRPCL] = (
-            "High-Throughput JPEG 2000 with RPCL Options Image Compression (Lossless Only)",
-            "Transfer Syntax",
-            "",
-            "",
-            "HTJ2KLosslessRPCL",
-        )
-        # PATCH/END: add HTJ2KLosslessRPCL to pydicom
-
-        if ds_file.file_meta.TransferSyntaxUID == HTJ2KLosslessRPCL:
-            ds_file.is_little_endian = True
-            ds_file.is_implicit_VR = False
-        ds_file.save_as(destination_file, write_like_original=False)
-        logger.debug(f"Saved DICOM dataset to {destination_file}")
-
-    res: List[str] = []
-    if altadb_meta_content_url.startswith("altadb:///"):
-        altadb_meta_content_url = "".join([base_url, "/", altadb_meta_content_url[10:]])
-    elif altadb_meta_content_url.startswith("altadb://"):
-        altadb_meta_content_url = altadb_meta_content_url.replace(
-            "altadb://", "https://"
-        )
-
-    async with aiohttp.ClientSession() as aiosession:
-        async with aiosession.get(altadb_meta_content_url, headers=headers) as response:
-            res_json = await response.json()
-            frameid_url_map: Dict[str, str] = {
-                frame["id"]: frame["path"] for frame in res_json.get("imageFrames", [])
-            }
-
-            tasks = []
-            for instance in res_json["metaData"]["instances"]:
-                frame_ids = [frame["id"] for frame in instance["frames"]]
-                image_frames_urls = [
-                    frameid_url_map[frame_id] for frame_id in frame_ids
-                ]
-                file_from_dataset_root = os.path.join(
-                    series_id, f"{instance['frames'][0]['id']}.dcm"
-                )
-                destination_filename = os.path.join(
-                    dataset_root, file_from_dataset_root
-                )
-                tasks.append(
-                    save_dicom_dataset(
-                        instance["metaData"],
-                        instance["frames"],
-                        image_frames_urls,
-                        destination_filename,
-                        aiosession,
-                    )
-                )
-                res.append(file_from_dataset_root)
-
-            await gather_with_concurrency(
-                MAX_CONCURRENCY,
-                tasks,
-                f"Saving series {series_dir.split('/')[-1]}",
-                keep_progress_bar=False,
-            )
-
-    return res
